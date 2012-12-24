66bba81378c6613bba22d4b3778d59f5a9b640a9
Ryan Clark <ryan.clark.j@gmail.com>
2012-12-24 09:50:18 -0700

    Add post_save signal to create UserProfiles automatically

--- models/profile.py
+++ models/profile.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     """Persist a user's profile information"""
@@ -12,8 +13,13 @@ class UserProfile(models.Model):
     
     # Geospatial components of the model
     objects = models.GeoManager()
+
+# This bit automatically generates a user profile for each created user
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)        
+post_save.connect(create_user_profile, sender=User)
     
 class UserProfileAdmin(admin.ModelAdmin):
     pass
-
 admin.site.register(UserProfile, UserProfileAdmin)
