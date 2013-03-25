e35d37b53d7607440b91782eb3631c2df9fd99af
David <david.dave.clark@gmail.com>
2013-03-24 19:43:20 -0700

    Tidied up Route model and started Trailhead model.

--- models/trailhead.py
+++ models/trailhead.py
@@ -1,14 +1,26 @@
 from django.contrib.gis.db import models
 from django.contrib.gis import admin
+from django.db.models import Q
+from trailguide.models import Route
 
 class Trailhead(models.Model):
     """Point features representing trailheads"""
     class Meta:
         app_label = 'trailguide'
+        
+    name = models.CharField(max_length=255)
     
     # Geospatial components of the model 
-    geo = models.PointField()
+    geo = models.PointField(geography=True, srid=4326)
     objects = models.GeoManager()
+    
+    def routes(self):
+        """
+        Return a list of routes that use this trailhead
+        as a starting or ending point.
+        """
+        return Route.objects.filter(Q(th_start=self) | Q(th_end=self))
+
     
 class TrailheadAdmin(admin.OSMGeoAdmin):
     pass
